41a9d182517a722e593aefd4b9c49a7c1689dc61
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-09-04 16:58:22 +0200

    ignore the .cache folder
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- packages/example/eslint.config.js
+++ packages/example/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.cache', '**/playwright/.cache/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
